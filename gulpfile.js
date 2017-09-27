082f62737f48f0a1ccd3968a1898ab82a0cd9c3a
boniverski <bosko.rabrenovic@gmail.com>
2017-09-27 12:46:15 +0200

    about section refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,28 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var bs = require('browser-sync').create();
-var autoprefixer = require('gulp-autoprefixer');
+const gulp         = require('gulp');
+const sass         = require('gulp-sass');
+const bs           = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
 
-// Copy ALL HTML files
-//gulp.task('copyHtml', function () {
-//gulp.src('src/*.html')
-//    .pipe(gulp.dest('public'));
-//});
-
-
+// SASS COMPILER WITH CSS PREFIXER
 gulp.task('sass', function () {
     return gulp.src('scss/*.scss')
                 .pipe(sass())
-		.pipe(autoprefixer({browsers: ['last 2 versions']}))
+		            .pipe(autoprefixer({browsers: ['last 2 versions']}))
                 .pipe(gulp.dest('css'))
                 .pipe(bs.reload({stream: true}));
 });
 
-// Copy ALL JS files
-//gulp.task('copyJs', function() {
-//  gulp.src('src/*.js')
-//      .pipe(gulp.dest('public'))
-//      .pipe(bs.reload({stream: true}));
-//});
-
+//BROWSER-SYNC HANDLING
 gulp.task('browser-sync', ['sass'], function() {
     bs.init({
         server: {
-            baseDir: "./"
+            baseDir: './'
         }
     });
 });
 
-gulp.task('watch', ['browser-sync'], function () {
-    gulp.watch("scss/*.scss", ['sass']);
-    gulp.watch("index.html").on('change', bs.reload);
+//GULP RUN
+gulp.task('default', ['browser-sync'], function () {
+    gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch('index.html').on('change', bs.reload);
 });
