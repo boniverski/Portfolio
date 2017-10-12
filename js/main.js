55c727140f2eba75c580a01bb590d03bd447b263
Boško Rabrenović <bosko.rabrenovic@gmail.com>
2017-10-12 17:22:49 +0200

    project collapse on sm devices

--- js/main.js
+++ js/main.js
@@ -4,3 +4,41 @@ new Vue({
   	showNav: false
   }
 });
+
+$(document).ready(function() {
+  const $hiddenColumn = $('section div#column-collapse');
+  const $btnCollapse = $('#btn-collapse');
+
+  $btnCollapse.on('click', function() {
+    $hiddenColumn.toggleClass('is-hidden-mobile');
+    //Toggle button text
+    $(this).text(function(i, text){
+      return text === "Show More" ? "Show Less" : "Show More";
+    });
+  });
+
+});
+// $(document).ready(function(){
+//   const mqSmall = window.matchMedia( '(max-width: 769px)' );
+//   const mqLarge = window.matchMedia( '(min-width: 770px)' );
+//   const button = '<button id="button-collapse" class="button">Show More</button>';
+//   const $projectContainer = $("#project-container");
+//   const $hiddenColumn = $(".hidden-sm");
+//
+//   // Check media query
+//   if(mqSmall.matches) {
+//
+//     $projectContainer.append(button);
+//     // Toggle appended button with id #button-collapse
+//     $("#button-collapse").on('click', function(){
+//       $hiddenColumn.toggleClass('hidden-sm');
+//       //Toggle button text
+//       $(this).text(function(i, text){
+//         return text === "Show More" ? "Show Less" : "Show More";
+//       });
+//     });
+//
+//   } else if (mqLarge.matches) {
+//     $("#button-collapse").css("display", "none");
+//   }
+// });
