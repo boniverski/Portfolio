9c82dee2e899eab45c6d6a73f459024f8382f95a
Boško Rabrenović <bosko.rabrenovic@gmail.com>
2017-10-17 18:12:11 +0200

    toogle nav menu - hide function

--- js/main.js
+++ js/main.js
@@ -1,14 +1,10 @@
-new Vue({
-  el: '#app',
-  data: {
-  	showNav: false
-  }
-});
-
 $(document).ready(function() {
   const $hiddenColumn = $('section div#column-collapse');
   const $btnCollapse = $('#btn-collapse');
+  const $hamburger = $('.nav span.nav-toggle');
+  const $navMenu = $('.nav .nav-right.nav-menu');
 
+  // SHOW/HIDE LAST THREE PROJECT TILES ON SMALL SCREENS
   $btnCollapse.on('click', function() {
     $hiddenColumn.toggleClass('is-hidden-mobile');
     //Toggle button text
@@ -17,39 +13,15 @@ $(document).ready(function() {
     });
   });
 
-  const $hamburger = $('.nav span.nav-toggle');
-  const $navMenu = $('.nav .nav-right.nav-menu');
-
-  $hamburger.on('click', function() {
-    $navMenu.toggleClass('is-visible');
-    //Toggle button text
-    // $(this).text(function(i, text){
-    //   return text === "Show More" ? "Show Less" : "Show More";
-    // });
+  // SHOW/HIDE NAV MENU ON SMALL SCREENS
+  $hamburger.on('click', function(e) {
+      $navMenu.toggleClass('is-visible');
+      e.stopPropagation();
   });
-
+  // Hiding Nav Menu clicking anywhere on screen
+  $(document).click(function(e) {
+    if ($(e.target).closest($navMenu).length == 0 ) {
+      $navMenu.removeClass('is-visible');
+    }
+  });
 });
-// $(document).ready(function(){
-//   const mqSmall = window.matchMedia( '(max-width: 769px)' );
-//   const mqLarge = window.matchMedia( '(min-width: 770px)' );
-//   const button = '<button id="button-collapse" class="button">Show More</button>';
-//   const $projectContainer = $("#project-container");
-//   const $hiddenColumn = $(".hidden-sm");
-//
-//   // Check media query
-//   if(mqSmall.matches) {
-//
-//     $projectContainer.append(button);
-//     // Toggle appended button with id #button-collapse
-//     $("#button-collapse").on('click', function(){
-//       $hiddenColumn.toggleClass('hidden-sm');
-//       //Toggle button text
-//       $(this).text(function(i, text){
-//         return text === "Show More" ? "Show Less" : "Show More";
-//       });
-//     });
-//
-//   } else if (mqLarge.matches) {
-//     $("#button-collapse").css("display", "none");
-//   }
-// });
