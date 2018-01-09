4073d79520bc497a48e6387d32b10062308f3e5e
boniverski <bosko.rabrenovic@gmail.com>
2018-01-09 11:16:09 +0100

    json projects

--- js/main.js
+++ js/main.js
@@ -1,19 +1,63 @@
 $(document).ready(function() {
-  const $hiddenColumn = $('section div#column-collapse');
-  const $btnCollapse = $('#btn-collapse');
-  const $hamburger = $('.nav span.nav-toggle');
-  const $navMenu = $('.nav .nav-right.nav-menu');
+
+  // FETCH PROJECT FROM PROJECS.JSON
+  fetch('./js/projects.json')
+    .then(response => response.json())
+    .then(data => {
+
+      const projects = data.projects,
+            width = $(window).width();
+      //First three projects always visible
+      for(id = 0; id < 3; id++) {
+        const projectTab = `
+                            <figure class="column project-tile">
+                              <img class="image" src=${projects[id].image} alt="Portfolio project">
+                              <div class="overlay">
+                                <h3>${projects[id].title}</h3>
+                                <div>
+                                  ${projects[id].tags.join(" &#8226; ")}
+                                </div>
+                                <div class="text"></div>
+                                <a href="${projects[id].link}">Demo</a>
+                              </div>
+                            </figure>
+                           `
+        $('.column-visible').append(projectTab);
+      }
+      //Last three projects hidden on small screens
+      for(id = 3; id < 6; id++) {
+        const projectTab = `
+                            <figure class="column project-tile">
+                              <img class="image" src=${projects[id].image} alt="Portfolio project">
+                              <div class="overlay">
+                                <h3>${projects[id].title}</h3>
+                                <div>
+                                  ${projects[id].tags.join(" &#8226; ")}
+                                </div>
+                                <div class="text"></div>
+                              </div>
+                            </figure>
+                           `
+        $('#column-collapse').append(projectTab);
+      }
+    });
 
   // SHOW/HIDE LAST THREE PROJECT TILES ON SMALL SCREENS
+  const $btnCollapse = $('#btn-collapse'),
+        $hiddenColumn = $('section div#column-collapse');
+
   $btnCollapse.on('click', function() {
     $hiddenColumn.toggleClass('is-hidden-mobile');
-    //Toggle button text
+
     $(this).text(function(i, text){
       return text === "Show More" ? "Show Less" : "Show More";
     });
   });
 
   // SHOW/HIDE NAV MENU ON SMALL SCREENS
+  const $hamburger = $('.nav span.nav-toggle'),
+        $navMenu = $('.nav .nav-right.nav-menu');
+
   $hamburger.on('click', function(e) {
       $navMenu.toggleClass('is-visible');
       e.stopPropagation();
@@ -24,4 +68,5 @@ $(document).ready(function() {
       $navMenu.removeClass('is-visible');
     }
   });
+
 });
